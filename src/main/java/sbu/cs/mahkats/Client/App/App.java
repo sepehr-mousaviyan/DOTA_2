34e7378f2f275a26e8b4329ddfeec8f823032df9
katayoun kobraei <katayoun.kobraei@gmail.com>
2021-06-02 16:56:33 +0430

    fix App-StartMethod

--- src/main/java/sbu/cs/mahkats/Client/App/App.java
+++ src/main/java/sbu/cs/mahkats/Client/App/App.java
@@ -10,7 +10,11 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Scene scene = new Scene(root);
 
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
