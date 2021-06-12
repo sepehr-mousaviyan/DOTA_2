93c95002879c061908d7d416048c65e3b8e8db1c
sepehr-mousaviyan <mousaviyan.sepehr@gmail.com>
2021-06-12 18:40:32 +0430

    add basics

--- src/main/java/sbu/cs/mahkats/Server/Model/Unit/Movable/Movable.java
+++ src/main/java/sbu/cs/mahkats/Server/Model/Unit/Movable/Movable.java
@@ -3,6 +3,6 @@ package sbu.cs.mahkats.Server.Model.Unit.Movable;
 import sbu.cs.mahkats.Server.Model.Unit.Unit;
 
 public class Movable extends Unit {
-    
+    //TODO move
 
 }
--- src/main/java/sbu/cs/mahkats/Server/Model/Unit/Unit.java
+++ src/main/java/sbu/cs/mahkats/Server/Model/Unit/Unit.java
@@ -11,4 +11,72 @@ public class Unit {
     int Location_x;
     int Location_y;
 
+    //TODO regenerate hp
+    public void Hp_regenerate() {
+        hp  = hp  + hp_regeneration;
+    }
+    //TODO damage giving
+
+    //TODO damage taking
+    public void takeDamage(Unit Opponent) {
+        int damageDealt = Opponent.getDamage();
+        hp  = hp  - (damageDealt - armor);
+    }
+
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getHp_regeneration() {
+        return hp_regeneration;
+    }
+
+    public void setHp_regeneration(int hp_regeneration) {
+        this.hp_regeneration = hp_regeneration;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public void setRange(int range) {
+        this.range = range;
+    }
+
+    public int getLocation_x() {
+        return Location_x;
+    }
+
+    public void setLocation_x(int location_x) {
+        Location_x = location_x;
+    }
+
+    public int getLocation_y() {
+        return Location_y;
+    }
+
+    public void setLocation_y(int location_y) {
+        Location_y = location_y;
+    }
 }
