39dc81bf5ae62771bf634e8ca291aa649f23710d
mahdi <esmokes17@gmail.com>
2021-06-27 21:23:12 +0430

    add hero list message

--- src/main/java/sbu/cs/mahkats/Api/MassageMaker.java
+++ src/main/java/sbu/cs/mahkats/Api/MassageMaker.java
@@ -5,6 +5,8 @@ import com.google.gson.JsonObject;
 import org.javatuples.Pair;
 import sbu.cs.mahkats.Api.Data.*;
 
+import java.util.ArrayList;
+
 public class MassageMaker{
 
     public JsonObject massage(String status, String action, UserData user){
@@ -48,5 +50,16 @@ public class MassageMaker{
     public JsonObject massage(String status, String action){
         return new Api().toJson(new Pair<>("status" , status),
                 new Pair<>("action" , action));
+    }
+
+    public JsonObject massage(String status, String action, int number, HeroData ...heroData){
+        Pair<String, String> statusProperties = new Pair<>("status" , status);
+        Pair<String, String> actionProperties = new Pair<>("action" , action);
+        ArrayList<Pair<String, JsonObject>> heroList = new ArrayList<>();
+        for(int i = 0 ; i < number; i++){
+            heroList.add(new Pair<>("hero" + i, heroData[i].makeJson()));
+        }
+        Pair contentProperties = new Pair<String, Pair>("content" , new Pair("heloList" , heroList));
+        return new Api().toJson(statusProperties, actionProperties, contentProperties);
     }
 }
